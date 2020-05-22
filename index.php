8b0dc24fbdc872cb00b376b4dfee48b43933261d
zaycev-cpu <sergeyols43@gmail.com>
2020-05-22 18:40:17 +0300

    index.php: add read settings file and init sycookie instance

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php 
+require_once 'sycookie.php';
 
+$settings = file_get_contents('settings.json');
+$settings = json_decode($settings);
+
+$sycookie = new SyCookie($settings->net, $settings->cookie_name);
