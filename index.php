ee5bd820537006f7677ae963d2e99c3bbb47bc46
zaycev-cpu <sergeyols43@gmail.com>
2020-05-23 00:46:45 +0300

    settings: settings file now has the extension .php

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <?php 
+require_once 'settings.php';
 require_once 'sycookie.php';
 require_once 'tools.php';
 
 require_once './actions/get.php';
 require_once './actions/put.php';
 
-define('VAL_SRC_LEN', 128);
-
-$settings = file_get_contents('settings.json');
-$settings = json_decode($settings);
-$sycookie = new SyCookie($settings->net, $settings->cookie_name);
+$sycookie = new SyCookie(SETTINGS_NET, SETTINGS_COOKIE_NAME);
 $sycookie->add_route("get", new GetAction());
 $sycookie->add_route("put", new PutAction());
 $sycookie->routing();
---
+++ settings.php
@@ -0,0 +1,5 @@
+<?php
+define('SETTINGS_NET', 'default');
+define('SETTINGS_COOKIE_NAME', 'sycookie_id');
+
+define('VAL_SRC_LEN', 128);
