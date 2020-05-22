5d270c7633b721878a4291524fea51d0297d47f3
zaycev-cpu <sergeyols43@gmail.com>
2020-05-23 00:32:48 +0300

    index: add routers and routing call

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php 
 require_once 'sycookie.php';
+require_once 'tools.php';
+
+require_once './actions/get.php';
+require_once './actions/put.php';
+
+define('VAL_SRC_LEN', 128);
 
 $settings = file_get_contents('settings.json');
 $settings = json_decode($settings);
-
 $sycookie = new SyCookie($settings->net, $settings->cookie_name);
+$sycookie->add_route("get", new GetAction());
+$sycookie->add_route("put", new PutAction());
+$sycookie->routing();
