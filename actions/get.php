90a2568c42623d7ba795d1a5024a1a85725df839
zaycev-cpu <sergeyols43@gmail.com>
2020-05-23 02:50:00 +0300

    actions/get: returns key && val

--- actions/get.php
+++ actions/get.php
@@ -4,13 +4,13 @@ require_once '_action.php';
 class GetAction extends BaseAction {
 	public function execute() {
 		$cn = $this->caller->cookie_name;
-		
+
 		if (!isset($_COOKIE[$cn])) {
 			$val = generate_value(VAL_SRC_LEN);
 			setcookie($cn, $val, time()+3600*24*365);
-			return $val;	
+			return array('val' => $val, 'key' => $cn);
 		}
 
-		return $_COOKIE[$cn];
+		return array('val' => $_COOKIE[$cn], 'key' => $cn);
 	}
 }
